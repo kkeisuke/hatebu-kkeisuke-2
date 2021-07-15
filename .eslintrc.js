ab87e6bd922e04d8390bebb37da08da3a3bd1541
kimura keisuke <mail@kkeisuke.com>
2021-07-15 18:34:55 +0900

    .eslintrc 見直し
    https://zenn.dev/kimromi/articles/b7cf98005f3193

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,6 @@ const config = {
     es2020: true,
     node: true
   },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier']
 }
 
